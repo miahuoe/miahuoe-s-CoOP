f3df843eadc881c78645504b154c71e130f1a826
Michał Czarnecki <czarnecky@va.pl>
2018-03-10 20:41:57 +0100

    Added fizzbuzz_00.c

---
+++ fizzbuzz_00.c
@@ -0,0 +1,14 @@
+/*
+ * An obfuscated "Fizz Buzz" program.
+ * Starts at 0, stops at N. Preferably N >= 0.
+ */
+
+#define N 100
+
+main(s,p,o,O,k,y){O=k=--s;for(s=s[""];s^N;s+=1,
+putchar('('>>!0>>(' '^'!'))){
+for(o=(~4681>>s%017&'s'/'s')<<2;o<4+(1057>>s%('/'&'O')&1)*+4;
+putchar("jk"[+o&'!']^",B0Q(>0Q"[o&'\'']&'>'/2+""[~-+1]),o++);
+if(2703*012>>s%15&1)for(p='d'*'d'*100;(p-':'+'1')>!1&&011<s||!
+putchar((k%2?k>>1&7:k/2%8)[k++&1?"16036158":"03725827"]+1);s/p?
+putchar('0'+s/p%10):O,p/='\n');}}
