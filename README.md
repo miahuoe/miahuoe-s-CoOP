c853ee5e1f66b8ea8d6620c6039749208865cac9
Michał Czarnecki <czarnecky@va.pl>
2018-03-16 15:28:03 +0100

    Added isprime_00.c. Updated README.md.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # miahuoe's Collection of Obfuscated Programs
-| File | Description | Characters |
-|:-|-|-:|
-| fizzbuzz\_00.c | "Fizz Buzz" with no special rules. All lowercase. Starts at 0. | 356 |
-(newlines are not counted)
+| File | Description |
+|:-|-|
+| fizzbuzz\_00.c | "Fizz Buzz" with no special rules. All lowercase. Starts at 0. |
+| isprime\_00.c | Checks if an integer given as argument is a prime number |
---
+++ isprime_00.c
@@ -0,0 +1,19 @@
+/*
+ * Prime tester.
+ * Supply any integer in arguments. Output:
+ * yes = argument is an integer and it's a prime number
+ * no = not a prime number
+ *      or not a valid integer
+ *      or no arguments
+ *      or more than one argument
+ */
+
+main(int a,char**v){
+int q[2][]={{0xf58c00f5,0x9186909a,0xf58c00f5,0},
+{0xf5008cf5,0x9a908691,0xf5008cf5,0}};
+char*g=q[**v&1]+1;int y=0[""""]>>-0,t=y,p,r,i,u=(*g^~0x65?2:-2);
+while(y=-+u,i=a--,v+=r=a--,!(p=a)){
+while(**v&&'/'<**v&&**v<':'&&(p*=5,p+=p,p+=*(*v)++&0xf,1));
+if(p^2||t++,p>' '>>4)for(;r?i<p||t++:0;r=p%i++?+1:'\0'[""]);}
+y+=t,u<0&&(y+='#'&'C')&&t?y-=2:0;
+while(y+=u,012^putchar(g[y]^-1));}
